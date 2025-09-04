f2480b578bf3c591b571da072ac0c4799fa03ea4
Nadendla, Sai Sailesh (SHS TE DC IND DI-D&A DEV IIT-RAD CRB IMG) <sai-sailesh.nadendla@siemens-healthineers.com>
2025-09-05 00:42:03 +0530

    work everything as pods

--- Source/FileUploader.Deployment/Deploy/Scripts/start.ps1
+++ Source/FileUploader.Deployment/Deploy/Scripts/start.ps1
@@ -8,10 +8,36 @@ function Main {
 
     Write-Host "Waiting for Redis pod to be up"
     do{
-        $pod = kubectl get pods -n file-uploader -l app=redis -o json | ConvertFrom-Json
+        $pod = kubectl get pods -n file-uploader -l app=fileuploader-redis -o json | ConvertFrom-Json
         if ($pod.items.Count -eq 0) { continue }
         $status = $pod.items[0].status.phase
     } while ($status -ne "Running")
+
+    Write-Host "Applying Server..."
+    kubectl apply -k "$scriptDir\..\Artifacts\Server"
+
+    Write-Host "Waiting for Server pod to be up"
+    do{
+        $pod = kubectl get pods -n file-uploader -l app=fileuploader-api -o json | ConvertFrom-Json
+        if ($pod.items.Count -eq 0) { continue }
+        $status = $pod.items[0].status.phase
+    } while ($status -ne "Running")
+
+    Write-Host "Applying Worker..."
+    kubectl apply -k "$scriptDir\..\Artifacts\Worker"
+
+    Write-Host "Waiting for Worker pod to be up"
+    do{
+        $pod = kubectl get pods -n file-uploader -l app=fileuploader-worker -o json | ConvertFrom-Json
+        if ($pod.items.Count -eq 0) { continue }
+        $status = $pod.items[0].status.phase
+    } while ($status -ne "Running")
+
+
+
+    Start-Process powershell -ArgumentList "kubectl port-forward deployment/fileuploader-api 5000:5000 -n file-uploader"
+    Start-Process powershell -ArgumentList "kubectl port-forward deployment/fileuploader-redis 6379:6379 -n file-uploader"
+
 
 
     Write-Host "All done!"
--- Source/FileUploader.Deployment/Deploy/Scripts/stop.ps1
+++ Source/FileUploader.Deployment/Deploy/Scripts/stop.ps1
@@ -3,13 +3,38 @@ function Main {
     $myInstscript = (Get-Item $PSCommandPath ).Basename
     Write-Host "Stop Command Invoked from $myInstscript"
 
+    $kubectlProcesses = Get-Process -Name "kubectl" -ErrorAction SilentlyContinue
+    $kubectlProcesses | ForEach-Object {
+        Write-Host "Stopping kubectl process with PID:" $_.Id
+        Stop-Process -Id $_.Id -Force
+    }
+
+
+     Write-Host "Deleting Server..."
+     kubectl delete -k "$scriptDir\..\Artifacts\Server"
+
+    Write-Host "Waiting for Server pod to be deleted"
+    do{
+         $pod = kubectl get pods -n file-uploader -l app=fileuploader-api -o json | ConvertFrom-Json
+    } while ($pod.items.Count -eq 0)
+
+
+
+    Write-Host "Deleting Worker..."
+    kubectl delete -k "$scriptDir\..\Artifacts\Worker"
+
+    Write-Host "Waiting for Worker pod to be down"
+    do{
+         $pod = kubectl get pods -n file-uploader -l app=fileuploader-worker -o json | ConvertFrom-Json
+    } while ($pod.items.Count -eq 0)
+
 
     Write-Host "Deleting Redis..."
     kubectl delete -k "$scriptDir\..\Artifacts\Redis"
 
     Write-Host "Waiting for Redis pod to be deleted"
     do{
-        $pod = kubectl get pods -n file-uploader -l app=redis -o json | ConvertFrom-Json
+        $pod = kubectl get pods -n file-uploader -l app=fileuploader-redis -o json | ConvertFrom-Json
     } while ($pod.items.Count -eq 0)
 
     Write-Host "All done!"
